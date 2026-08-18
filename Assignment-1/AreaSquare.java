678b44cb23eb1bab16d51ff163a7f0cf6770ad94
Mohammed Sameer Rangrej_AC <mohammedsameer.rangrej.cmaug26@gmail.com>
2026-08-18 17:48:08 +0530

    Add files via upload

---
+++ Assignment-1/AreaSquare.java
@@ -0,0 +1,7 @@
+public class AreaSquare {
+	public static void main(String args[]) {
+		int side = 5;
+		int area = side * side;
+		System.out.println("The area of the square is " + area + ".");		
+	}	
+}
